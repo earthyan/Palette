5c8ef737d2c80a079b4557cee1d4cc9c8eaeb7ef
kurotokiya <kurotokiya@gmail.com>
2015-09-02 18:26:24 +0800

    fix database seeder error

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Palette\Services\Registrar;
 
 class DatabaseSeeder extends Seeder {
   /**
@@ -14,5 +15,30 @@ class DatabaseSeeder extends Seeder {
 
     $this->call(UserSeeder::class);
 		$this->call(SystemSeeder::class);
+  }
+}
+
+class UserSeeder extends Seeder {
+  public function run(){
+    $data = [
+      'email' => 'admin@admin.com',
+      'password' =>'123456',
+      'name' => 'admin',
+      'group' => '1'
+    ];
+    $register = new Registrar();
+    $register->create($data);
+  }
+}
+
+class SystemSeeder extends Seeder {
+  public function run(){
+    $config = [
+      [
+        'key' => 'title',
+        'value' => 'Palette'
+      ],
+    ];
+    DB::table('setting')->insert($config);
   }
 }
