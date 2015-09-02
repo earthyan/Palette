ebcfec92a8fbdbf981d1237f36bda271b1b771a7
kurotokiya <kurotokiya@gmail.com>
2015-09-02 19:02:46 +0800

    add cdn table

---
+++ database/migrations/2015_09_02_190123_create_cdn_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCdnTable extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up(){
+    Schema::create('cdn', function (Blueprint $table) {
+      $table->increments('id');
+      $table->string('name');
+      $table->string('url');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down(){
+    Schema::drop('cdn');
+  }
+}
