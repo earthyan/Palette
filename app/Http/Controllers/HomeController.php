bbce84e7e6051acd88decbc0626917ce72f0ed7e
kurotokiya <kurotokiya@gmail.com>
2015-09-03 19:22:25 +0800

    temp home page

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,7 @@
+<?php namespace Palette\Http\Controllers;
+
+class HomeController extends Controller {
+  public function home(){
+    return view('home');
+  }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+  <ol class="breadcrumb">
+    <li><a href="/home"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a></li>
+    <li class="active">首页</li>
+  </ol>
+@endsection
