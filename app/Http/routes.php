8a70564401bf674fe37bbd766a347ca406ae1564
kurotokiya <kurotokiya@gmail.com>
2015-09-03 19:22:43 +0800

    update routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,6 +6,8 @@ Route::get('/', function (){
 
 Route::get('home', 'HomeController@home');
 
+Route::controller('img', 'ImgController');
+
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
