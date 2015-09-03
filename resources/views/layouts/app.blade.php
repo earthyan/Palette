d8341faa19250a05cff18dd71d362a50ced8779a
kurotokiya <kurotokiya@gmail.com>
2015-09-03 20:41:11 +0800

    add some meta to main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="renderer" content="webkit">
     <title>{{ \Palette\Helper::getTitle() }}</title>
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
   </head>
