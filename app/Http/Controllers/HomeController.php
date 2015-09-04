aeeb4bce6294e519ad8edaa67966087c19e97893
kurotokiya <kurotokiya@gmail.com>
2015-09-04 22:00:32 +0800

    new home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,23 @@
 <?php namespace Palette\Http\Controllers;
 
+use Auth;
+
 class HomeController extends Controller {
   public function __construct(){
     $this->middleware('auth', ['except' => 'link']);
   }
 
   public function home(){
-    return view('home');
+    $used = round(Auth::user()->disk_used/1024, 4);
+    $total = Auth::user()->disk_total;
+    $percent = round(($used / $total) * 100, 2);
+    $data = [
+      'used' => $used,
+      'total' => $total,
+      'percent' => $percent
+    ];
+
+    return view('home')->withData($data);
   }
 
   public function link($file){
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,4 +5,15 @@
     <li><a href="/home"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a></li>
     <li class="active">首页</li>
   </ol>
+
+  <div class="col-md-12">
+    <h1>欢迎使用 {{ \Palette\Helper::getTitle() }}！</h1>
+    <hr>
+    <h4>您已经使用的空间：{{ $data['used'] }}/{{ $data['total'] }} MB</h4>
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" aria-valuenow="{{ $data['percent'] }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $data['percent'] }}%;">
+        {{ $data['percent'] }}%
+      </div>
+    </div>
+  </div>
 @endsection
